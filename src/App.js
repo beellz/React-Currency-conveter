2a4c62220a38d56a3c646de354548b0802395ba0
beellz <belal20795@gmail.com>
2020-06-09 12:59:36 +0530

    changes with date time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import CurrencyRow from "./CurrencyRow";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Convert</h1>
+      <CurrencyRow />
+
     </div>
   );
 }
+
+//this is to check date time change
+
 
 export default App;
